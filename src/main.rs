35c0a9b6b9b7960a140e99c632fd1589d57ec0c4
muathendirangu <muathe.ndirangu@gmail.com>
2024-06-07 21:39:02 +0300

    define basic smart contract

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+pub struct NumberStorage {
+    a_favourite_number: u64,
+}
+
+impl NumberStorage {
+    pub fn new() -> Self {
+        Self {
+            a_favourite_number: 0,
+        }
+    }
+
+    pub fn store_number(&mut self, favourite_number: u64) {
+        self.a_favourite_number = favourite_number;
+    }
+
+    pub fn retrieve_number(&self) -> u64 {
+        self.a_favourite_number
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
